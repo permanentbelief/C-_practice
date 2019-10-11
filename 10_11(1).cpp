5ecf6fafc1b2a298a26debd2e4f0f1549c627503
permenent <2691556679@qq.com>
2019-10-11 23:44:43 +0800

    commit

---
+++ 10_11(1).cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+//#include <stdio.h>
+//int main()
+//{
+//	int tmp;
+//	int sum;
+//	for (int i = 1; i < 5; i++)
+//	{
+//		tmp = tmp * 10 + a;//10½øÖÆ
+//		sum += tmp;
+//	}
+//
+//	for (int i = n; i; i /= 10)
+//	{
+//		tmp = i % 10;
+//		sum = sum * 10 + tmp;
+//	}
+//	fanhui erjinzhi 1 de geshu
+//	for (i = value; i; i /= 2)
+//	{
+//		count += i % 2;
+//	}
+//}
+//#include <iostream>
+//using namespace std;
+//class MyClass
+//{
+//public:
+//	MyClass() { cout << 1; }
+//};
+//int main()
+//{
+//	MyClass a, b[2], *p[2];
+//	system("pause");
+//}
