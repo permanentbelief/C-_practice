3c19a3ff30987edb0d0acde8f0689ec007abfc14
permenent <2691556679@qq.com>
2019-10-15 22:56:38 +0800

    commit

---
+++ 15(1).c
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS 1 
+//#include <stdio.h>
+//int g(int m, int n)
+//{
+//	if (m == 0 && n >= 0)
+//	{
+//		return 0;
+//	}
+//	return g(m - 1, 2*n) + n;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入m和n的值\n");
+//	scanf("%d %d", &a, &b);
+//	if (a < 0 || b < 0)
+//	{
+//		printf("输入有误\n");
+//	}
+//	printf("%d",g(a,b));
+//	system("pause");
+//}
