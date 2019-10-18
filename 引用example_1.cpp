db1104f00783d2fb7e0a511e69becca631b4d1d7
permenent <2691556679@qq.com>
2019-10-18 21:22:06 +0800

    commit

---
+++ 引用example_1.cpp
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS 1 
+//#include <iostream>
+//using namespace std;
+//
+//void test(int a, int b = 2, int c = 3)
+//{
+//	cout << "a=" << a << endl;
+//	cout << "b=" << b << endl;
+//	cout << "c=" << c << endl;
+//}
+//int main()
+//{
+//	//test();
+//	test(1);
+//	system("pause");
+//}
