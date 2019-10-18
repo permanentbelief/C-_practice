74843cb543d2d07a97ccded9083972ad82f28229
permenent <2691556679@qq.com>
2019-10-18 21:22:40 +0800

    commit

---
+++ 引用example_2.cpp
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS 1 
+//#include <iostream>
+//using namespace std;
+//
+//int TestRefReturn(int a)
+//{
+//	a += 10;
+//	return a;
+//}
+//int main()
+//{
+//	int a = 4;
+//	int& b = a;
+//	cout << TestRefReturn(b) << endl;
+//	system("pause");
+//
+//}
