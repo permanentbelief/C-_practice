7c169389bc6ea1e37d4442e0015619fcc5479eb0
permenent <2691556679@qq.com>
2019-11-28 23:13:43 +0800

    commit

---
+++ 蓝桥杯T2.cpp
@@ -0,0 +1,54 @@
+//#define _CRT_SECURE_NO_WARNINGS 1 
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//using namespace std; 
+//vector<int> helper(vector<int> nums, int target)
+//{
+//	if (nums.empty())
+//	{
+//		return nums;
+//	}
+//	vector <int> v;
+//	int flag1 = 0;
+//	int flag2 = 0;
+//	for (int i = 0; i < nums.size(); i++)
+//	{
+//		for (int j = i + 1; j < nums.size(); j++)
+//		{
+//			if (nums[i] + nums[j] == target)
+//			{
+//				v[0] = i;
+//				v[1] = k
+//			}
+//		}
+//	}
+//	return v;
+//}
+//int main()
+//{
+//
+//	
+//	int target = 0;
+//	cout << "输入目标数" << endl;
+//	cin >> target;
+//	int nums = 0;
+//	cout << "输入数组的大小 k, 以及数组" << endl;
+//	cin >> nums;
+//	vector<int> v;
+//	for (int i = 0; i < nums; i++)
+//	{
+//		int j = 0;
+//		cin >> j;
+//		v.push_back(j);
+//	}
+//	
+//
+//	// 示例
+//	//vector<int> v({ 2, 7, 11, 15 });
+//	//vector<int> temp = helper(v, k);
+//	//cout << temp[0] << temp[1] << endl;
+//	vector<int> temp = helper(v, target);
+//	cout << temp[0]  << " " << temp[1] << endl;
+//	system("pause");
+//}
